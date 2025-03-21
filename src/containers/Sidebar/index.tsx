d54b0d2a14cf28b0893600484ae592717ae23d84
Evaldoer <evaldorode@gmail.com>
2025-03-21 19:15:58 -0300

    construindo a estrutura e a seção "sobre mim"

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/Evaldoer.png" />
+    <Titulo fontSize={20}>Evaldoer</Titulo>
+  </aside>
+)
+
+export default Sidebar
